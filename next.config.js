ad3a2f80ee06a13e28f9500c26897387a9167280
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-05-22 22:35:24 +0700

    chore : update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,48 @@
 /** @type {import('next').NextConfig} */
-import withLess from "next-with-less";
-// const withLess = require("next-with-less");
-const nextConfig = {};
+const nextConfig = {
+	// Configure the build process.
+	typescript: {
+		// Enable TypeScript support.
+		enable: true,
+	},
 
-// module.exports = withLess(nextConfig);
-export default withLess(nextConfig);
+	// Configure the routing system.
+	// routes: [
+	// 	{
+	// 		path: "/",
+	// 		component: MyHomePage,
+	// 	},
+	// 	{
+	// 		path: "/about",
+	// 		component: MyAboutPage,
+	// 	},
+	// ],
+
+	// Configure the SEO settings.
+	seo: {
+		title: "Charity App",
+		description: "This is my website.",
+	},
+
+	// Configure the analytics settings.
+	// analytics: {
+	// 	// Enable Google Analytics.
+	// 	googleAnalytics: {
+	// 		trackingId: "UA-XXXXXXXX-X",
+	// 	},
+	// },
+
+	// Configure the image optimization settings.
+	images: {
+		// Enable image optimization.
+		enable: true,
+	},
+
+	// Configure the font optimization settings.
+	fonts: {
+		// Enable font optimization.
+		enable: true,
+	},
+};
+
+module.exports = nextConfig;
