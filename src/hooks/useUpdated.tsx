e9bec24cc52fa7c93a47dabbb59fae1a2e4661ad
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-06-03 11:13:19 +0700

    feat : add useUpdated hooks

---
+++ src/hooks/useUpdated.tsx
@@ -0,0 +1,22 @@
+import {
+  type EffectCallback,
+  type DependencyList,
+  useEffect,
+  useRef,
+} from 'react'
+/*
+ * React hooks that run useEffect() hooks only when the dependency changes,
+ * not when the component is in initial mounted.
+ * @param callback - The callback to run only when the dependency changes
+ * @param deps - The dependencies to listen to
+ * @returns (void) - void */
+
+function useUpdated(callback: EffectCallback, deps: DependencyList): void {
+  const mounted = useRef(false)
+  useEffect(() => {
+    if (mounted.current) return callback()
+    mounted.current = true
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps)
+}
+export default useUpdated
