8b3d241cf507d914892d86c175f137a0bbb14bef
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-06-09 18:00:08 +0700

    chore : move signup page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,27 +5,31 @@ const Loading = () => {
   const [isLoading, setIsLoading] = useState(true)
 
   const handleLoading = () => {
-    console.log('calling handleLoading')
     setTimeout(() => {
       setIsLoading(false)
+      return () => window.removeEventListener('load', handleLoading)
     }, 1500)
   }
 
   useEffect(() => {
     window.addEventListener('load', handleLoading)
-    return () => window.removeEventListener('load', handleLoading)
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
   }, [])
+
   // console.log(isLoading)
-  return (
-    isLoading && (
-      <div id="preloader">
-        <div className="spinner">
-          <div className="double-bounce1" />
-          <div className="double-bounce2" />
-        </div>
+  return isLoading ? (
+    <div id="preloader">
+      <div className="spinner">
+        <div className="double-bounce1" />
+        <div className="double-bounce2" />
       </div>
-    )
-  )
+    </div>
+  ) : (
+    <></>
+  ) // return <></>
 }
 
 export default Loading
