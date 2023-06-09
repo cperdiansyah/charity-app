7ac599faab16832566625a95be0c8e51ac59a25e
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-06-09 14:41:30 +0700

    feat : add auth logic on desktop view

--- next.config.js
+++ next.config.js
@@ -2,51 +2,6 @@
 
 const path = require('path')
 const nextConfig = {
-  // Configure the build process.
-  // typescript: {
-  //   // Enable TypeScript support.
-  //   enable: true,
-  // },
-  // compress: false,
-
-  // Configure the routing system.
-  // routes: [
-  // 	{
-  // 		path: "/",
-  // 		component: MyHomePage,
-  // 	},
-  // 	{
-  // 		path: "/about",
-  // 		component: MyAboutPage,
-  // 	},
-  // ],
-
-  // Configure the SEO settings.
-  // seo: {
-  //   title: 'Charity App',
-  //   description: 'This is my website.',
-  // },
-
-  // // Configure the analytics settings.
-  // // analytics: {
-  // // 	// Enable Google Analytics.
-  // // 	googleAnalytics: {
-  // // 		trackingId: "UA-XXXXXXXX-X",
-  // // 	},
-  // // },
-
-  // // Configure the image optimization settings.
-  // images: {
-  //   // Enable image optimization.
-  //   enable: true,
-  // },
-
-  // // Configure the font optimization settings.
-  // fonts: {
-  //   // Enable font optimization.
-  //   enable: true,
-  // },
-  // Saass
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
---
+++ src/app/user/profile/page.tsx
@@ -0,0 +1,12 @@
+import UserLayout from 'components/templates/UserLayout'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <UserLayout>
+      <div>Profile</div>
+    </UserLayout>
+  )
+}
+
+export default Profile
