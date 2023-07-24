4b6d52041ef1e2b95225f367f4e441ba423b0542
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-07-24 07:16:55 +0700

    feat : integrate submit user request

--- src/app/campaign/ApprovalUser.tsx
+++ src/app/campaign/ApprovalUser.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { AuditOutlined } from '@ant-design/icons'
-import { Form, Modal, Spin } from 'antd'
+import { Alert, Form, Modal, Spin } from 'antd'
 import _isEmpty from 'lodash/isEmpty'
 import _get from 'lodash/get'
 
@@ -11,23 +11,75 @@ import { SERVICE } from '@/utils/api'
 import { api } from '@/utils/clientSideFetch'
 import { IErrorResponse } from '@/services/auth/index.interface'
 import { notify } from '@/helpers/notify'
+import useTextEditor from '@/stores/textEditor'
+import QuilEditor from '@/components/molecules/QuilEditor'
 
 interface IApprovalUser {
   className: string
+}
+
+export interface IApprovalDataUser {
+  userVerified: boolean
+  approvalData: null | ApprovalData
+  userApprovalData: any
+}
+
+export interface ApprovalData {
+  _id: string
+  status: string
+  approval_type: string
+  foreign_id: string
+  refModel: string
+  createdAt: string
+  updatedAt: string
+  __v: number
+  foreign_data: ForeignData
+}
+
+export interface ForeignData {
+  _id: string
+  name: string
+  username: string
+  email: string
+  role: string
+  is_verified: boolean
+  createdAt: string
+  updatedAt: string
+  __v: number
 }
 
 const ApprovalUser = (props: IApprovalUser) => {
   const [form] = Form.useForm()
 
   const [userData, setUserData] = useUserData()
+  const [editorValue, setEditorValue] = useTextEditor()
+  const [errorEditor, setErrorEditor] = useState(false)
+
   const [loading, setLoading] = useState(true)
   const [loadingSubmit, setLoadingSubmit] = useState(false)
   const [isModalOpen, setIsModalOpen] = useState(false)
-  const [approvalData, setApprovalData] = useState({
+  const [approvalData, setApprovalData] = useState<IApprovalDataUser>({
     userVerified: false,
-    status: 'pending',
+    approvalData: null,
     userApprovalData: null,
   })
+
+  const editorEmptyLogic =
+    editorValue.length === 0 ||
+    editorValue === '<p><br></p>' ||
+    editorValue === '<p></p>'
+
+  useEffect(() => {
+    handleResetForm()
+  }, [])
+
+  useEffect(() => {
+    if (errorEditor) {
+      if (!editorEmptyLogic) {
+        setErrorEditor(false)
+      }
+    }
+  }, [editorValue.length])
 
   useEffect(() => {
     if (userData?.is_verified === false) {
@@ -35,6 +87,7 @@ const ApprovalUser = (props: IApprovalUser) => {
     }
   }, [userData.id])
 
+  /* Fetching Data */
   const getApprovalUser = async () => {
     try {
       if (userData.id) {
@@ -55,7 +108,10 @@ const ApprovalUser = (props: IApprovalUser) => {
           ...user,
           userVerified: userData.is_verified,
           status: dataApproval.data.status,
-          approvalData: !_isEmpty(dataApprovalUser) ? dataApprovalUser : null,
+          approvalData: !_isEmpty(dataApproval) ? dataApproval.data : null,
+          userApprovalData: !_isEmpty(dataApprovalUser)
+            ? dataApprovalUser
+            : null,
         }))
       }
       setLoading(false)
@@ -65,6 +121,7 @@ const ApprovalUser = (props: IApprovalUser) => {
     }
   }
 
+  /* Modal Function */
   const showModal = () => {
     setIsModalOpen(true)
   }
@@ -77,11 +134,37 @@ const ApprovalUser = (props: IApprovalUser) => {
     setIsModalOpen(false)
   }
 
+  /* Submit Function */
+
   const handleSubmitRequest = async (values: any) => {
     setLoadingSubmit(true)
     try {
+      const dataSubmitRequest = {
+        user_id: userData.id,
+        approval_id: approvalData.approvalData?._id,
+        description: editorValue,
+      }
+      if (_isEmpty(approvalData.userApprovalData)) {
+        await api.post(`${SERVICE.ApprovalUser}/create`, dataSubmitRequest)
+      } else {
+        await api.patch(
+          `${SERVICE.ApprovalUser}/update/${approvalData.userApprovalData._id}`,
+          dataSubmitRequest
+        )
+      }
+
       handleReset()
       setLoadingSubmit(false)
+
+      const titleNoiify = _isEmpty(approvalData.userApprovalData)
+        ? 'Campaign Submit Request Successful'
+        : 'Update Campaign Submit Request Successful'
+      notify(
+        'success',
+        titleNoiify,
+        'please wait for it to be reviewed by the admin',
+        'bottomRight'
+      )
     } catch (error) {
       const resError: IErrorResponse = _get(error, 'response.data.error', {
         code: 400,
@@ -98,25 +181,33 @@ const ApprovalUser = (props: IApprovalUser) => {
       )
     }
   }
+
+  /* Form Function */
   const handleReset = () => {
     setIsModalOpen(false)
     form.resetFields()
+  }
+  const handleResetForm = () => {
+    setEditorValue('')
+    form.resetFields()
+  }
+
+  const checkEditorValue = () => {
+    if (editorEmptyLogic) {
+      setErrorEditor(true)
+    }
   }
 
   console.log(approvalData)
   const userNotRequestCampaign =
     !approvalData.userVerified &&
-    approvalData.status === 'pending' &&
+    approvalData?.approvalData?.status === 'pending' &&
     approvalData.userApprovalData === null
   const userRequestCampaignButNotApprovedYet =
     !approvalData.userVerified &&
-    approvalData.status === 'pending' &&
+    approvalData?.approvalData?.status === 'pending' &&
     approvalData.userApprovalData !== null
 
-  const title = userNotRequestCampaign
-    ? 'Make a Campaign Request'
-    : userRequestCampaignButNotApprovedYet &&
-      'Your request is being reviewed, please wait'
   return (
     <div className={`${[props.className].join(' ')}`}>
       <Spin tip="Loading" size="small" spinning={loading}>
@@ -153,7 +244,14 @@ const ApprovalUser = (props: IApprovalUser) => {
                   onFinish={handleSubmitRequest}
                   autoComplete="off"
                   layout="vertical"
+                  onFinishFailed={checkEditorValue}
                 >
+                  <Form.Item>
+                    <QuilEditor placeholder="Give us a reason why you want to create a campaign" />
+                    {errorEditor && (
+                      <Alert message="Text Editor is Required" type="error" />
+                    )}
+                  </Form.Item>
                   <Form.Item>
                     <CustomButton
                       buttontype="primary"
