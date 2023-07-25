ed2cc4f7900208d2f67a9f16969ea495c42b4aa6
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2023-07-25 21:43:50 +0700

    feat : initial commit campaign moderation

--- src/app/admin/moderation/campaign/page.tsx
+++ src/app/admin/moderation/campaign/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { Button, Descriptions, Image, Modal, Space, Tag, Tooltip } from 'antd'
 import dayjs from 'dayjs'
+import _get from 'lodash/get'
 import { EditOutlined, InfoCircleOutlined } from '@ant-design/icons'
-import Link from 'next/link'
 import React, { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 
@@ -11,34 +11,56 @@ import CustomTable from '@/components/organisms/Table'
 
 /* Utils */
 import { currencyFormat } from '@/helpers'
-import { getCharityClient } from '@/services/charity/clientService'
-import { NAVIGATION_LINK } from '@/utils/link'
 import useUpdated from '@/hooks/useUpdated'
+import { getApprovalCharity } from '@/services/moderation/clientService'
+import { IModalTable } from '../../campaign/campaign.interfce'
+import { CAMPAIGN_STATUS_WITH_COLORS } from '../../campaign/campaign'
 
 function getColumns(showModal: any) {
   return [
     {
-      dataIndex: 'title',
+      dataIndex: ['foreign_data', 'title'],
       key: 'title',
       title: 'Title',
       width: 250,
     },
     {
+      title: 'Status',
+      dataIndex: ['foreign_data', 'status'],
+      key: 'status',
+      width: 100,
+      render: (status: any) => {
+        let campaignStatus = CAMPAIGN_STATUS_WITH_COLORS.find(
+          (item) => item.status === status
+        )
+
+        if (campaignStatus) {
+          return (
+            <Tag color={campaignStatus?.color}>
+              {campaignStatus?.status.toUpperCase()}
+            </Tag>
+          )
+        }
+
+        return <Tag color="default">{status.toUpperCase()}</Tag>
+      },
+    },
+    {
       title: 'Donation Target',
-      dataIndex: 'donation_target',
+      dataIndex: ['foreign_data', 'donation_target'],
       key: 'donation_target',
 
-      render: (data: number) => `Rp. ${currencyFormat(data)}`,
+      render: (data: number) => `${currencyFormat(data)}`,
     },
     {
       title: 'Start Date',
-      dataIndex: 'start_date',
+      dataIndex: ['foreign_data', 'start_date'],
       key: 'start_date',
       render: (date: string) => dayjs(date).format('DD MMMM YYYY'),
     },
     {
       title: 'End Date',
-      dataIndex: 'end_date',
+      dataIndex: ['foreign_data', 'end_date'],
       key: 'end_date',
       render: (date: string) => dayjs(date).format('DD MMMM YYYY'),
     },
@@ -50,13 +72,13 @@ function getColumns(showModal: any) {
       render: (value: any, record: any) => {
         return (
           <div className="flex items-center">
-            <Tooltip placement="bottomRight" title="Edit Banner">
-              <Link
+            <Tooltip placement="bottomRight" title="Approve Campaign">
+              {/* <Link
                 href={`${NAVIGATION_LINK.AdminCampaignEdit}${value._id}`}
                 className="px-3 py-2"
               >
-                <EditOutlined />
-              </Link>
+            </Link> */}
+              <EditOutlined />
             </Tooltip>
             <Tooltip placement="bottomRight" title="Detail Banner">
               <Button
@@ -73,8 +95,125 @@ function getColumns(showModal: any) {
   ]
 }
 
+const MemoizeModalTable = React.memo(ModalTable)
+
 const ModerationCampaign = () => {
-  return <div>ModerationCampaign</div>
+  const searchParams = useSearchParams()
+  const [visible, setVisible] = useState<boolean>(false)
+  const [campaignData, setCampaignData] = useState<any>()
+  const current = searchParams.get('current')
+  const pageSize = searchParams.get('pageSize')
+  const queryParams = {
+    page: current || 1,
+    rows: pageSize || 10,
+  }
+  const init = async () => {
+    const dataCharity = await getApprovalCharity(queryParams)
+    const result = {
+      data: dataCharity.data,
+      meta: dataCharity.meta,
+    }
+    console.log(result)
+    return result
+  }
+  const showModal = (record: any) => {
+    setCampaignData(record)
+    setVisible(true)
+  }
+
+  return (
+    <div>
+      <CustomTable columns={getColumns(showModal)} init={init} />
+      <MemoizeModalTable
+        open={visible}
+        setOpen={setVisible}
+        data={campaignData}
+        setData={setCampaignData}
+      />
+    </div>
+  )
+}
+
+function ModalTable(props: IModalTable) {
+  if (props?.data === undefined) return <></>
+
+  const handleCancel = (e: any) => {
+    e.stopPropagation()
+    if (props?.setOpen) props?.setOpen(false)
+  }
+
+  const resetData = (e?: any) => {
+    setTimeout(() => {
+      if (props?.setData) props?.setData()
+    }, 500)
+  }
+
+  useUpdated(() => {
+    if (!props.open) {
+      resetData()
+    }
+  }, [props.open])
+
+  /* Status */
+  const { status } = props?.data?.foreign_data
+  let campaignStatus = CAMPAIGN_STATUS_WITH_COLORS.find(
+    (item) => item.label === status
+  )
+
+  return (
+    <Modal
+      onCancel={handleCancel}
+      open={props.open}
+      footer={null}
+      closable={true}
+      maskClosable={true}
+    >
+      {/* <Button>Detail</Button> */}
+      <Descriptions bordered className="mt-4">
+        <Descriptions.Item label="Title" span={24}>
+          {props?.data?.foreign_data?.title}
+        </Descriptions.Item>
+        <Descriptions.Item label="Image" span={24}>
+          <Image
+            src={_get(props?.data, 'foreign_data.media[0].content')}
+            alt={props?.data?.foreign_data?.title}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </Descriptions.Item>
+        <Descriptions.Item label="Donation Target" span={24}>
+          {`${currencyFormat(props?.data?.foreign_data?.donation_target)}`}
+        </Descriptions.Item>
+        <Descriptions.Item label="Status" span={24}>
+          {campaignStatus ? (
+            <Tag color={campaignStatus?.color}>
+              {campaignStatus?.status?.toUpperCase()}
+            </Tag>
+          ) : (
+            <Tag color="default">{status?.toUpperCase()}</Tag>
+          )}
+        </Descriptions.Item>
+
+        <Descriptions.Item label="Start Date" span={24}>
+          {dayjs(props?.data?.foreign_data?.start_date).format('DD MMMM YYYY')}
+        </Descriptions.Item>
+        <Descriptions.Item label="End Date" span={24}>
+          {dayjs(props?.data?.foreign_data?.end_date).format('DD MMMM YYYY')}
+        </Descriptions.Item>
+        <Descriptions.Item label="Post Date" span={24}>
+          {props?.data?.foreign_data?.post_date
+            ? dayjs(props?.data?.foreign_data?.post_date).format('DD MMMM YYYY')
+            : '-'}
+        </Descriptions.Item>
+        <Descriptions.Item label="Description" span={24}>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: props?.data?.foreign_data?.description,
+            }}
+          ></div>
+        </Descriptions.Item>
+      </Descriptions>
+    </Modal>
+  )
 }
 
 export default ModerationCampaign
---
+++ src/services/moderation/clientService.ts
@@ -0,0 +1,30 @@
+import _get from 'lodash/get'
+import { SERVICE } from '@/utils/api'
+import { api } from '@/utils/clientSideFetch'
+
+interface IGetApprovalCampaignQuery {
+  page?: number | string
+  rows?: number | string
+  // refModel?: refModel
+}
+type refModel = 'User' | 'Charity' | 'Banner'
+
+export const getApprovalClient = async (
+  query?: IGetApprovalCampaignQuery,
+  refModel?: refModel
+) => {
+  try {
+    const resCharity = await api.get(`${SERVICE.Approval}/list`, {
+      params: { ...query, refModel },
+    })
+    const dataCharity = _get(resCharity, 'data')
+    return dataCharity
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
+
+export const getApprovalCharity = async (query?: IGetApprovalCampaignQuery) => {
+  const dataApprovalCharity = await getApprovalClient(query, 'Charity')
+  return dataApprovalCharity
+}
